a7e118828754f854f3503ad5130f4017743d7146
imrankrm <mailtomdimu@gmail.com>
2019-02-02 16:19:04 +0530

    Working on login page

---
+++ src/components/login.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Login Page Working...</p>
+            </div>
+        )
+    }
+}
+
+export default Login;
